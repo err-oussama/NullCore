7f529ed5a5939604c5611bf7f3f38b90edb21cf6
oussama errami <oussama0errami@gmail.com>
2026-05-29 02:45:47 +0100

    feat: implement PIT init function and PIT header file

--- kernel/drivers/pit.c
+++ kernel/drivers/pit.c
@@ -1,12 +1,23 @@
+#include "pmio.h"
 #include "type.h"
 #include <pic.h>
 #include <pit.h>
 
-uint64 ticks = 0;
-
-uint64 pit_get_tick() { return ticks; }
+static uint64 ticks = 0;
 
 void timer_handler() {
   ticks++;
   pic_send_eoi(0);
+}
+
+uint64 pit_get_tick() { return ticks; }
+
+void pit_init(uint32 frequency) {
+  uint32 divisor32 = 1193182 / frequency;
+  uint16 divisor = (uint16)divisor32;
+
+  outb(PIT_COMMAND, PIT_MODE_ACCESS_LOHIBYTE | PIT_MODE_OP_MODE_2);
+
+  outb(PIT_CHANNEL0, divisor & 0xFF);
+  outb(PIT_CHANNEL0, (divisor >> 0x8) & 0xFF);
 }
--- kernel/drivers/pit.h
+++ kernel/drivers/pit.h
@@ -3,7 +3,35 @@
 #include <type.h>
 // Programmable Interval Timer
 
+#define PIT_MODE_CHANNEL_0 0x00
+#define PIT_MODE_CHANNEL_1 0x40
+#define PIT_MODE_CHANNEL_2 0x80
+#define PIT_MODE_CHANNEL_RBC 0xC0 // Read Back Command
+
+#define PIT_MODE_ACCESS_LATCH 0x00    // latch count value
+#define PIT_MODE_ACCESS_LOBYTE 0x10   // low byte only
+#define PIT_MODE_ACCESS_HIBYTE 0x20   // high byte only
+#define PIT_MODE_ACCESS_LOHIBYTE 0x30 // low byte then high byte
+
+#define PIT_MODE_OP_MODE_0 0x00 // Interrupt on terminal count
+#define PIT_MODE_OP_MODE_1 0x02 // hardware re-triggerable on shot
+#define PIT_MODE_OP_MODE_2 0x04 // rate generator
+#define PIT_MODE_OP_MODE_3 0x06 // square wave generator
+#define PIT_MODE_OP_MODE_4 0x08 // software triggered strobe
+#define PIT_MODE_OP_MODE_5 0x0A // hardware triggered strobe
+
+#define PIT_MODE_NUMBER_FORMAT_BINARY 0x00
+#define PIT_MODE_NUMBER_FORMAT_BCD 0x01 // Binary Coded Decimal
+
+#define PIT_CHANNEL0 0x40 // Timer channel connected to IRQ0
+#define PIT_CHANNEL1 0x41 // oboslete in modern system
+#define PIT_CHANNEL2 0x42 // PC speaker
+
+#define PIT_COMMAND 0x43
+
 void timer_handler();
 uint64 pit_get_tick();
+
+void pit_init(uint32 frequency);
 
 #endif
