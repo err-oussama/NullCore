8b930ef4f85b60006c946a289034a1138e58c776
oussama errami <oussama0errami@gmail.com>
2026-05-31 03:28:22 +0100

    feat: implement cpu_context saver and loader, for general purpos registers only

--- kernel/kernel/kmain.c
+++ kernel/kernel/kmain.c
@@ -3,18 +3,25 @@
 #include <memory.h>
 #include <multiboot_metadata.h>
 #include <pit.h>
+#include <task.h>
 
 void kmain(multiboot_info *boot_info) {
   kclear_screen();
   init_pmp(boot_info);
   setup_hardware();
   init_heap();
+  cpu_context context;
+  cpu_context new;
+  new.eax = 0xAA;
+  new.ebx = 0xBB;
+  new.ecx = 0xCC;
+  new.edx = 0xDD;
+  new.edi = 0x99;
+  new.esi = 0x55;
+  context.eax = 0xff;
+  load_context(&context);
+  save_context(&new);
 
-  uint64 start = pit_get_tick();
-  for (uint32 i = 0; i < 60; i++) {
-    start = pit_get_tick();
-    while (pit_get_tick() - start < 1000)
-      ;
-    kprintf("%d!! ", i + 1);
-  }
+  kprintf("eax: %x\nebx: %x\necx: %x\nedx: %x\nedi: %x\nesi: %x\n", new.eax,
+          new.ebx, new.ecx, new.edx, new.edi, new.esi);
 }
