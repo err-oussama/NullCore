fa60ab259feaa1d9e8a680326c0b7fcbff78894b
oussama errami <oussama0errami@gmail.com>
2026-07-01 02:27:48 +0100

    feat: read CR2 to get the address causes page fault exception to fire

--- kernel/cpu/control_registers.h
+++ kernel/cpu/control_registers.h
@@ -5,10 +5,11 @@
 
 // Control Registers
 
-void write_cr0(uint32 cr0_v);
+void write_cr0(uint32 cr0_v); // store flags
 uint32 read_cr0();
 
-void write_cr3(uint32 cr3_v);
+void write_cr3(uint32 cr3_v); // CR3 store the page directoy pointer
 uint32 read_cr3();
 
+uint32 read_cr2(); // store the cause page fault
 #endif
