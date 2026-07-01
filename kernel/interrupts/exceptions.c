108790a2465e3667819caa459a1412107106536b
oussama errami <oussama0errami@gmail.com>
2026-07-01 02:26:31 +0100

    feat: page fault exception now show the address that cause page fault to fire

--- kernel/interrupts/exceptions.c
+++ kernel/interrupts/exceptions.c
@@ -1,48 +1,49 @@
 #include "isr.h"
 #include "kprint.h"
+#include <control_registers.h>
 
 void divide_error_handler() {
-  kprint_err("Interrupt: Division Error ");
+  kprint_err("Exception: Division Error ");
   while (1)
     ;
 }
 void debug_handler() {
-  kprint_err("Interrupt: Debug");
+  kprint_err("Exception: Debug");
   while (1)
     ;
 }
 void non_maskable_interrupt_handler() {
-  kprint_err("Interrupt: Non Maskable Interrupt");
+  kprint_err("Exception: Non Maskable Interrupt");
   while (1)
     ;
 }
 void break_point_handler() {
-  kprint_err("Interrupt: Break Point");
+  kprint_err("Exception: Break Point");
   while (1)
     ;
 }
 void overflow_handler() {
-  kprint_err("Interrupt: overflow");
+  kprint_err("Exception: overflow");
   while (1)
     ;
 }
 void bound_range_exceeded_handler() {
-  kprint_err("Interrupt: Bound Range Exceeded");
+  kprint_err("Exception: Bound Range Exceeded");
   while (1)
     ;
 }
 void invalid_opcode_handler() {
-  kprint_err("Interrupt: Invalid OPcode");
+  kprint_err("Exception: Invalid OPcode");
   while (1)
     ;
 }
 void device_not_avalibale_handler() {
-  kprint_err("Interrupt: Device Not Available");
+  kprint_err("Exception: Device Not Available");
   while (1)
     ;
 }
 void double_fault_handler() {
-  kprint_err("Interrupt:  Double Fault");
+  kprint_err("Exception:  Double Fault");
   while (1)
     ;
 }
@@ -52,72 +53,74 @@ void coprocessor_segment_overrun_handler() {
     ;
 }
 void invalid_TSS_handler() {
-  kprint_err("Interrupt: Invalid TSS");
+  kprint_err("Exception: Invalid TSS");
   while (1)
     ;
 }
 void segment_not_present_handler() {
-  kprint_err("Interrupt: Segment Not Present");
+  kprint_err("Exception: Segment Not Present");
   while (1)
     ;
 }
 void stack_segment_fault_handler() {
-  kprint_err("Interrupt: Stack Segment Fault");
+  kprint_err("Exception: Stack Segment Fault");
   while (1)
     ;
 }
 void general_protection_fault_handler() {
-  kprint_err("Interrupt: General Protection Fault");
+  kprint_err("Exception: General Protection Fault");
   while (1)
     ;
 }
 void page_fault_handler() {
-  kprint_err("Interrupt: Page Fault");
+  kprint_err("Exception: Page Fault [0x");
+  kprint_hex(read_cr2());
+  kprint_err("]");
   while (1)
     ;
 }
 void x87_floating_point_exception_handler() {
-  kprint_err("Interrupt: X87 Floating Point Exception");
+  kprint_err("Exception: X87 Floating Point Exception");
   while (1)
     ;
 }
 void alignment_check_handler() {
-  kprint_err("Interrupt: Alignment Check");
+  kprint_err("Exception: Alignment Check");
   while (1)
     ;
 }
 void machine_check_handler() {
-  kprint_err("Interrupt: Machine Check");
+  kprint_err("Exception: Machine Check");
   while (1)
     ;
 }
 void SIMD_floating_point_exception_handler() {
-  kprint_err("Interrupt: SIMD Floating Point Exception");
+  kprint_err("Exception: SIMD Floating Point Exception");
   while (1)
     ;
 }
 void virtualization_exception_handler() {
-  kprint_err("Interrupt: Virtualization Exception");
+  kprint_err("Exception: Virtualization Exception");
   while (1)
     ;
 }
 void control_protection_exception_handler() {
-  kprint_err("Interrupt: Control Protection Exception");
+  kprint_err("Exception: Control Protection Exception");
   while (1)
     ;
 }
 void hypervisor_injection_exception_handler() {
-  kprint_err("Interrupt: Hypervisor Injection Exception");
+  kprint_err("Exception: Hypervisor Injection Exception");
   while (1)
     ;
 }
 void VMM_communication_exception_handler() {
-  kprint_err("Interrupt: VMM Communication Exception");
+  kprint_err("Exception: VMM Communication Exception");
   while (1)
     ;
 }
 void security_exception_handler() {
-  kprint_err("Interrupt: Security Exception");
+  kprint_err("Exception: Security Exception");
   while (1)
     ;
 }
