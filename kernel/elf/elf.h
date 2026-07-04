d44b6a53936f09f77bc4638d865f2b00265613da
oussama errami <oussama0errami@gmail.com>
2026-07-04 03:26:25 +0100

    feat: add macros for the ELF header specificly for identification array

--- kernel/elf/elf.h
+++ kernel/elf/elf.h
@@ -1,6 +1,33 @@
 #ifndef ELF_H
 #define ELF_H
 
-#include <type.h>
+#include <types.h>
+
+/*
+ *  EI  		=> ELF Identification
+ *  NIDENT 	=> Number of Identification Bytes
+ *
+ *
+ */
+#define EI_NIDENT 0x10
+
+typedef struct elf_s elf_t;
+
+struct elf_s {
+  uint8 ident[EI_NIDENT];
+  uint16 type;
+  uint16 machine;
+  uint16 version;
+  uint32 entry;
+  uint32 phoff; // program header offset
+  uint32 shoff; // segment header offset
+  uint32 flags;
+  uint16 ehsize;
+  uint16 phentsize;
+  uint16 phnum;
+  uint16 shentsize;
+  uint16 shnum;
+  uint16 shstrndx;
+};
 
 #endif
---
+++ kernel/elf/elf_header_macro.h
@@ -0,0 +1,16 @@
+#ifndef ELF_HEADER_MACRO_H
+#define ELF_HEADER_MACRO_H
+
+#define EI_MAG0 0x0
+#define EI_MAG1 0x1
+#define EI_MAG2 0x2
+#define EI_MAG3 0x3
+#define EI_CLASS 0x4 // Architecture? 32-bit or 64-bit
+#define EI_DATA 0x5  // data encoding? Little-endian or Big-endian
+#define EI_VERSION 0x6
+#define EI_OSABI 0x7      // OS ABI? ARM or LINUX or STANDALONE or ...
+#define EI_ABIVERSION 0x8 // ABI version
+#define EI_PAD 0x9
+#define EI_NIDENT 0xA // size of the elf_t array
+
+#endif
