7048fa9ebd4247fd0c2cd07117c3651f56d6efa8
oussama errami <oussama0errami@gmail.com>
2026-07-11 02:52:50 +0100

    fix: rename the syscall_not_implemented function to syscall_nothing

--- kernel/interrupts/syscall.c
+++ kernel/interrupts/syscall.c
@@ -1,14 +1,14 @@
 #include "syscall.h"
 #include <kprint.h>
 
-void syscall_not_implemented(registers *regs) {
-  kprint_str("syscall coming soon\n");
-}
+void sys_nothing(registers *regs) { kprint_str("syscall coming soon\n"); }
 
-syscall_handler syscall_table[] = {
-    syscall_not_implemented, syscall_not_implemented, syscall_not_implemented,
-    syscall_not_implemented, syscall_not_implemented, syscall_not_implemented,
-    syscall_not_implemented, syscall_not_implemented, syscall_not_implemented};
+void sys_read(registers *regs) { kprint_str("Read syscall coming soon\n"); }
+
+void sys_write(registers *regs) { kprint_str("Write syscall coming soon\n"); }
+syscall_handler syscall_table[] = {sys_read,    sys_write,   sys_nothing,
+                                   sys_nothing, sys_nothing, sys_nothing,
+                                   sys_nothing, sys_nothing, sys_nothing};
 
 void syscall_dispatch(uint32 edi, uint32 esi, uint32 ebp, uint32 ESP,
                       uint32 ebx, uint32 edx, uint32 ecx, uint32 eax) {
--- user/user_program_asm.asm
+++ user/user_program_asm.asm
@@ -1,6 +1,6 @@
 global elf_syscall
 
 elf_syscall:
-	mov eax, 0
+	mov eax, 0x1
 	int 0x80
 	ret 
