89432a74f59c528232b819d36974bc3d2c288783
oussama errami <oussama0errami@gmail.com>
2026-07-18 03:05:21 +0100

    style: begin filesystem

--- kernel/core/kmain.c
+++ kernel/core/kmain.c
@@ -1,23 +1,8 @@
-#include <control_registers.h>
-#include <elf.h>
 #include <kernel.h>
-#include <kheap.h>
-#include <kprint.h>
-#include <multiboot_metadata.h>
-#include <pit.h>
-#include <registers.h>
-#include <syscall.h>
-#include <task.h>
-#include <tss.h>
-
-#include <syscall.h>
 
 void task_mock() { kprintf("t"); }
 
 void kmain(multiboot_info *boot_info) {
   init_kernel(boot_info);
-  kprint_wrn("======== [ Working on: Syscall write ] ========\n");
-  void *elf = initrd_start;
-
-  load_elf(elf);
+  kprint_wrn("======== [ Working on: File System ] ========\n");
 }
