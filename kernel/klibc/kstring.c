7503e01e818b5512d9e2a40b8dfa27cb97347e51
oussama errami <oussama0errami@gmail.com>
2026-07-27 03:23:12 +0100

    feat: add strlen, bezero

--- kernel/klibc/kstring.c
+++ kernel/klibc/kstring.c
@@ -1,3 +1,5 @@
+#include "sys/types.h"
+#include "types.h"
 #include <kstring.h>
 
 void memset(void *addr, uint32 c, uint32 size) {
@@ -6,6 +8,8 @@ void memset(void *addr, uint32 c, uint32 size) {
     p[size] = c;
   }
 }
+
+void bezero(void *addr, uint32 size) { memset(addr, 0, size); }
 
 void memcpy(void *src, void *dest, uint32 size) {
   uint8 *s = src;
@@ -19,11 +23,18 @@ void memcpy(void *src, void *dest, uint32 size) {
   if (src > dest) {
     i = -1;
     while (++i < size)
-      s[i] = d[i];
+      d[i] = s[i];
 
   } else {
     i = size;
     while (i--)
-      s[i] = d[i];
+      d[i] = s[i];
   }
+}
+
+uint32 strlen(char *str) {
+  uint32 len = 0;
+  while (str[len])
+    len++;
+  return len;
 }
--- kernel/klibc/kstring.h
+++ kernel/klibc/kstring.h
@@ -4,5 +4,7 @@
 
 void memset(void *addr, uint32 c, uint32 size);
 void memcpy(void *src, void *dest, uint32 size);
+uint32 strlen(char *str);
+void bezero(void *addr, uint32 size);
 
 #endif
