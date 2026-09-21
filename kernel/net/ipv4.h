53d7500a79fe8bb290427de0260261361f0015c5
oussama errami <oussama0errami@gmail.com>
2026-09-21 03:22:13 +0000

    docs: add a helper explain for the ver_ihl field

--- kernel/net/ipv4.h
+++ kernel/net/ipv4.h
@@ -6,18 +6,24 @@
 // IP options are not supported;
 // header length (IHL) is expected to always be 5 (20-byte header, no options)
 typedef struct __attribute__((packed)) {
-  uint8 ver_ihl; // version (4 bits) + header length (4 bits)
-  uint8 tos;     // type of service
+
+  // version (4 bits) + header length (4 bits) in uints of 32-bit words
+  uint8 ver_ihl;
+  uint8 tos; // type of service
+
   uint16 total_len;
 
-  uint16 id;         // identification
+  uint16 id; // identification
+
   uint16 flags_frag; // flags (3 bits) + fragment offset (13 bits)
 
-  uint8 ttl; // time to live
-  uint8 protocol;
+  uint8 ttl;      // time to live
+  uint8 protocol; // TCP, UDP, ...
+
   uint16 checksum; // header checksum
 
   uint8 src_ip[4];
+
   uint8 dest_ip[4];
 
   uint8 data[];
